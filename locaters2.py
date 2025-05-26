c33fd9d6e373bfde6e1335fb5c8c7f0aa62af8e6
Varunreddy <varunsannapureddy@gmail.com>
2025-05-26 15:38:49 +0530

    modified:   locaters2.py

--- locaters2.py
+++ locaters2.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome(service=service_obj)
 driver.get("https://www.facebook.com/")
 driver.maximize_window()
 
-driver.find_element(By.CSS_SELECTOR,"input#email").send_keys("Pavan kalyan")
-driver.find_elements(By.CSS_SELECTOR,"input.inputtext")[1].send_keys("kaljd")
+driver.find_element(By.CSS_SELECTOR,"input#email").send_keys("Varunreddy")
+driver.find_elements(By.CSS_SELECTOR,"input.inputtext")[1].send_keys("Varun")
 
 time.sleep(10)
